bed0092416d62022d759721aa14cbc30acfa4009
ktns <pom-2551@hotmail.com>
2022-01-24 20:49:04 +0700

    chore: added mapview and polyline decoder

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ThemedText from "../../components/themed-text";
 import { useTheme } from "@react-navigation/native";
+import MapView, { Polyline } from "react-native-maps";
+import { decode } from "@googlemaps/polyline-codec";
 
 export default function Home() {
     const { colors } = useTheme();
@@ -17,10 +19,30 @@ export default function Home() {
             fontSize: 32,
             fontWeight: "bold",
         },
+        maps: {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+        },
     });
+
+    function decodePolyline() {
+        const decoded = decode(polyline, 5);
+        console.log(decoded);
+        let decodedPolyline = [];
+
+        decoded.forEach((element) => {
+            decodedPolyline.push({ latitude: element[0], longitude: element[1] });
+        });
+
+        return decodedPolyline;
+    }
 
     return (
         <View style={styles.container}>
+            {/* <MapView style={styles.maps}></MapView> */}
             <ThemedText style={styles.text}>Hello</ThemedText>
         </View>
     );
