a49cd6eb00bb96cde628992d53f0d8a5bc93be69
Napat Asavarojkul <puttersnap@icloud.com>
2022-02-04 21:31:15 +0700

    fix: add searchbox

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import { Text} from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SearchBox from "../../components/search-box";
 import ThemedText from "../../components/themed-text";
 
 export default function Search() {
-  return (<><ThemedText>Hello</ThemedText></>);
+    const styles = StyleSheet.create({
+        container: { padding: 12 },
+    });
+    return (
+        <SafeAreaView style={styles.container}>
+            <SearchBox />
+        </SafeAreaView>
+    );
 }
