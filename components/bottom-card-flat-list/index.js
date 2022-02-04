13e00f214a2f7b81b76adc695f76d7b0eb5995d0
ktns <pom-2551@hotmail.com>
2022-02-04 20:50:15 +0700

    chore: refactor codes

--- components/bottom-card-flat-list/index.js
+++ components/bottom-card-flat-list/index.js
@@ -1,46 +1,55 @@
 import React from "react";
-import { TouchableOpacity, SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
+import {
+    TouchableOpacity,
+    SafeAreaView,
+    View,
+    FlatList,
+    StyleSheet,
+    Text,
+    StatusBar,
+    Image,
+} from "react-native";
 import ThemedText from "../themed-text";
 import BusIcon from "../../assets/bus-icon";
 import ArrowIcon from "../../assets/arrow_forward-icon";
 
 const DATA = [
     {
-      id: '1',
-      title: 'Victory Monument',
-      line: '333',
-      time: 10
+        id: "1",
+        title: "Victory Monument",
+        line: "333",
+        time: 10,
     },
     {
-      id: '2',
-      title: 'Rangsit',
-      line: '333',
-      time: 10
+        id: "2",
+        title: "Rangsit",
+        line: "333",
+        time: 10,
     },
     {
-      id: '3',
-      title: 'Lam Lukka',
-      line: '333',
-      time: 10
+        id: "3",
+        title: "Lam Lukka",
+        line: "333",
+        time: 10,
     },
     {
-        id: '4',
-        title: 'Lam Lukka',
-        line: '59',
-        time: 10
-      },
-  ];
-  
-  const Item = ({ title, time, line }) => (
+        id: "4",
+        title: "Lam Lukka",
+        line: "59",
+        time: 10,
+    },
+];
+
+const Item = ({ title, time, line }) => (
     <TouchableOpacity style={styles.lower_container}>
         <View style={styles.sub_container}>
             <View style={styles.transport}>
-                <BusIcon style={styles.icon}/>
+                <BusIcon style={styles.icon} />
                 <ThemedText style={styles.line}>{line}</ThemedText>
             </View>
-            <ArrowIcon style={styles.arrow}/>
+            <ArrowIcon style={styles.arrow} />
             <View style={styles.item}>
-            <ThemedText style={styles.title}>{title}</ThemedText>
+                <ThemedText style={styles.title}>{title}</ThemedText>
             </View>
         </View>
         <View style={styles.timeSection}>
@@ -48,47 +57,38 @@ const DATA = [
             <ThemedText style={styles.time}>min</ThemedText>
         </View>
     </TouchableOpacity>
-  );
-
+);
 
 export default function BottomCardFlatList() {
-
     // const { colors } = useTheme();
 
-    const renderItem = ({ item }) => (
-        <Item title={item.title} time={item.time} line={item.line}/>
-      );
+    const renderItem = ({ item }) => <Item title={item.title} time={item.time} line={item.line} />;
 
     const ItemDivider = () => {
-        return (
-          <View style={styles.devider}/>
-        );
-      }
-    
-      return (
-        <SafeAreaView style={styles.container}>
-          <FlatList
-            data={DATA}
-            renderItem={renderItem}
-            keyExtractor={item => item.id}
-            ItemSeparatorComponent={ItemDivider}
-          />
-        </SafeAreaView>
-      );
+        return <View style={styles.devider} />;
+    };
 
-};
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={ItemDivider}
+            />
+        </SafeAreaView>
+    );
+}
 
 const styles = StyleSheet.create({
-    lower_container:{
+    lower_container: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
         backgroundColor: "#282828",
         padding: 10,
-        // marginVertical: 1,
-        // marginHorizontal: 5,
     },
-    sub_container:{
+    sub_container: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "flex-start",
@@ -98,57 +98,50 @@ const styles = StyleSheet.create({
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
-      },
-    container: {
-    // height:300,
-    //   flex: 1,
-    //   marginTop: StatusBar.currentHeight || 0,
     },
+    container: {},
     transport: {
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-around",
         backgroundColor: "white",
         width: 80,
-        borderRadius:5,
+        borderRadius: 5,
     },
     icon: {
         width: 25,
         height: 25,
-        // marginRight:8,
-        marginLeft:4,
-        marginTop:8,
-        marginBottom:6,
+        marginLeft: 4,
+        marginTop: 8,
+        marginBottom: 6,
     },
     line: {
-      marginTop:2,
-      fontSize: 25,
-      color: "red",
+        marginTop: 2,
+        fontSize: 25,
+        color: "red",
     },
-    arrow : {
-        marginLeft:5,
-        marginRight:5,
+    arrow: {
+        marginLeft: 5,
+        marginRight: 5,
     },
     title: {
-      fontSize: 18,
-      color: "white",
+        fontSize: 18,
+        color: "white",
     },
-    timeSection:{
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "flex-end",
+    timeSection: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
     },
     time: {
-      fontSize: 25,
-      marginRight:2,
-      marginTop:3,
-      color: "white",
-      textAlign: "right"
+        fontSize: 25,
+        marginRight: 2,
+        marginTop: 3,
+        color: "white",
+        textAlign: "right",
     },
     devider: {
         height: 0.5,
         backgroundColor: "gray",
-        // marginHorizontal: 5,
-    }
-
-  });
+    },
+});
