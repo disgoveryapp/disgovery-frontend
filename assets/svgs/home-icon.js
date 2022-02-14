23b3cba465d6fd84c94549687dc38781ebfa3668
kritasak <jojokritasak@gmail.com>
2022-02-14 13:59:46 +0700

    fix: svgs home/work/location size

--- assets/svgs/home-icon.js
+++ assets/svgs/home-icon.js
@@ -6,11 +6,11 @@ const HomeIcon = (props) => {
     const { colors } = useTheme();
 
     return (
-        <Svg width={23} height={22} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <Svg width={27} height={27} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
             <Path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M0 11.2 11.458 0l11.459 11.2h-3.438v9.954h-6.875v-7.466h-2.291v7.466H3.436v-9.955H0Zm17.188-2.253-5.73-5.6-5.729 5.6v9.718h2.292V11.2h6.875v7.466h2.291V8.947Z"
+                d="M0 14.64 13.5.875 27 14.64h-4.05v12.235h-8.1v-9.177h-2.7v9.177h-8.1V14.64H0Zm20.25-2.769L13.5 4.99l-6.75 6.882v11.945h2.7V14.64h8.1v9.176h2.7V11.872Z"
                 fill={props.fill || colors.text}
             />
         </Svg>
