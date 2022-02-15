80f2c6dce574a2a68a82f7a53ebaf3e031e9ef5d
Napat Asavarojkul <puttersnap@icloud.com>
2022-02-15 23:55:33 +0700

    fix: showing content (not finished)

--- pages/search/index.js
+++ pages/search/index.js
@@ -17,39 +17,17 @@ import PlaceTab from "./components/place-tab";
 import { useDebounce } from "use-lodash-debounce";
 import StationTab from "./components/station-tab";
 import { configs } from "../../configs/configs";
+import TransportName from "../../components/transport-name";
+import BadConnectionComponent from "./components/bad-connection";
 
 export default function Search() {
     const { colors } = useTheme();
     const [text, onChangeText] = useState("");
-    const [api22Result, setApi22Result] = useState([
-        {
-            location: {
-                coords: {
-                    lat: "",
-                    lon: "",
-                },
-            },
-            name: {
-                en: "",
-                th: "",
-            },
-            station_id: "",
-        },
-    ]);
-    const [api21Result, setApi21Result] = useState([
-        {
-            address: {
-                en: "",
-                th: "",
-            },
-            name: {
-                en: "",
-                th: "",
-            },
-            place_id: "",
-        },
-    ]);
-    const debouncedValue = useDebounce(text, 300);
+    const [api22Result, setApi22Result] = useState([]);
+    const [api21Result, setApi21Result] = useState([]);
+    const debouncedValue = useDebounce(text, 100);
+    const [error21, setError21] = useState(false);
+    const [error22, setError22] = useState(false);
 
     useEffect(() => {
         const simpleApi22Call = async (str) => {
@@ -57,25 +35,30 @@ export default function Search() {
                 method: "get",
                 url: `${configs.API_URL}/autocomplete/stations?query=${text}&max_result=4`,
                 headers: {},
-            });
+            }).catch(setError22(true));
+            setError22(false);
             setApi22Result(result.data.data);
         };
+        simpleApi22Call();
+        console.log(api22Result);
+    }, [text]);
+
+    /*useEffect(() => {
         const simpleApi21Call = async (str) => {
             const result = await axios({
                 method: "get",
                 url: `${configs.API_URL}/autocomplete/places?query=${text}`,
                 headers: {},
-            });
+            }).catch(setError21(true));
+            setError21(false);
             if (debouncedValue) {
                 setApi21Result(result.data);
             }
         };
-        console.log(text);
-        simpleApi22Call();
+
         simpleApi21Call();
-        console.log(api22Result);
-        console.log(api21Result);
     }, [debouncedValue]);
+    */
 
     const styles = StyleSheet.create({
         searchbox: {
@@ -125,39 +108,51 @@ export default function Search() {
                 <TouchableOpacity onPress={() => onChangeText}></TouchableOpacity>
 
                 <View style={styles.scrollView}>
-                    <ScrollView
-                        style={styles.scrollView}
-                        showsHorizontalScrollIndicator={false}
-                        showsVerticalScrollIndicator={false}
-                        contentContainerStyle={{ paddingBottom: 35, paddingTop: 12 }}
-                    >
-                        {api22Result !== undefined && api21Result !== undefined ? (
-                            <>
-                                <View>
-                                    <ThemedText style={styles.topictext}>Stations</ThemedText>
-                                    <View style={styles.tabbarcontainer}>
-                                        {api22Result !== undefined ? (
-                                            <>
-                                                {api22Result.map((item, key) => (
-                                                    <StationTab
-                                                        key={key}
-                                                        icon="train"
-                                                        place={item.name.en}
-                                                        type="train"
-                                                    ></StationTab>
-                                                ))}
-                                            </>
-                                        ) : (
-                                            <></>
-                                        )}
-                                    </View>
-                                </View>
-
-                                <View>
-                                    <ThemedText style={styles.topictext}>Places</ThemedText>
-                                    <View style={styles.tabbarcontainer}>
-                                        {api21Result !== undefined ? (
-                                            <>
+                    {error21 && error22 ? (
+                        <BadConnectionComponent />
+                    ) : (
+                        <ScrollView
+                            style={styles.scrollView}
+                            showsHorizontalScrollIndicator={false}
+                            showsVerticalScrollIndicator={false}
+                            contentContainerStyle={{ paddingBottom: 35, paddingTop: 12 }}
+                            bounces={false}
+                        >
+                            {(api22Result !== undefined &&
+                                api22Result !== null &&
+                                api22Result.length !== 0) ||
+                            (api21Result !== undefined &&
+                                api21Result !== null &&
+                                api21Result.length !== 0) ? (
+                                <>
+                                    {api22Result !== undefined &&
+                                    api22Result !== null &&
+                                    api22Result.length !== 0 ? (
+                                        <>
+                                            <ThemedText style={styles.topictext}>
+                                                Stations
+                                            </ThemedText>
+                                            <View style={styles.tabbarcontainer}>
+                                                <>
+                                                    {api22Result.map((item, key) => (
+                                                        <StationTab
+                                                            key={key}
+                                                            place={item.name.en}
+                                                            trip={item.trips}
+                                                        ></StationTab>
+                                                    ))}
+                                                </>
+                                            </View>
+                                        </>
+                                    ) : (
+                                        <></>
+                                    )}
+                                    {api21Result !== undefined &&
+                                    api21Result !== null &&
+                                    api21Result.length !== 0 ? (
+                                        <View>
+                                            <ThemedText style={styles.topictext}>Places</ThemedText>
+                                            <View style={styles.tabbarcontainer}>
                                                 {api21Result.map((item, key) => (
                                                     <PlaceTab
                                                         key={key}
@@ -165,28 +160,22 @@ export default function Search() {
                                                         address={item.address.en}
                                                     ></PlaceTab>
                                                 ))}
-                                            </>
-                                        ) : (
-                                            <></>
-                                        )}
-                                    </View>
-                                </View>
-                            </>
-                        ) : (
-                            <>
-                                <View>
-                                    <ThemedText style={styles.topictext}>
-                                        Favourite location
-                                    </ThemedText>
-                                </View>
-                                <View>
-                                    <ThemedText style={styles.topictext}>Recent</ThemedText>
-                                </View>
-                            </>
-                        )}
-                    </ScrollView>
+                                            </View>
+                                        </View>
+                                    ) : (
+                                        <></>
+                                    )}
+                                </>
+                            ) : (
+                                <></>
+                            )}
+                        </ScrollView>
+                    )}
                 </View>
             </View>
         </>
     );
 }
+/*
+{item.name.en}
+*/
