2a89940384d12d158f76e7a83dd6003015de00d7
kritasak <jojokritasak@gmail.com>
2022-03-18 11:38:49 +0700

    Feat: LineTab (No Props)

---
+++ pages/search/components/line-tab/index.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import ThemedText from "../../../../components/themed-text";
+import PlaceIcon from "../../../../assets/svgs/place-icon";
+import { useTheme } from "@react-navigation/native";
+import BusIcon from "../../../../assets/svgs/bus-icon";
+import SubwayIcon from "../../../../assets/svgs/subway-icon";
+import TramIcon from "../../../../assets/svgs/tram-icon";
+import BoatIcon from "../../../../assets/svgs/boat-icon";
+import RailIcon from "../../../../assets/svgs/rail-icon";
+import { color } from "react-native-elements/dist/helpers";
+
+function LineTab(props) {
+    const { colors } = useTheme();
+
+    const styles = StyleSheet.create({
+        container: {
+            width: "100%",
+            height: 71,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            paddingHorizontal: 15,
+            paddingVertical: 12,
+            borderBottomColor: colors.divider,
+            borderBottomWidth: 1,
+        },
+        topContainer: {
+            flex: 1,
+            width: "100%",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+        },
+        dataContainer: {
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "center",
+        },
+        iconContainer: {
+            width: 27,
+            height: 27,
+            borderRadius: 6,
+            backgroundColor: colors.text,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        titleContainer: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: 21,
+            backgroundColor: "#1E4F6F", //use props
+            borderRadius: 6,
+            paddingLeft: 6,
+            paddingRight: 6,
+            marginLeft: 6,
+        },
+        title: {
+            fontSize: 14,
+            color: colors.text,
+        },
+        distance: {
+            fontSize: 16,
+            color: colors.subtitle,
+        },
+        bottomContainer: {
+            flex: 1,
+            width: "100%",
+            flexDirection: "row",
+            alignItems: "flex-end",
+            justifyContent: "flex-start",
+        },
+        nearestSubtitle: {
+            fontSize: 14,
+            color: colors.subtitle,
+        },
+        locationSubtitle: {
+            fontSize: 16,
+            color: colors.text,
+        },
+    });
+
+    return (
+        <TouchableOpacity style={styles.container} onPress={props.onPress}>
+            <View style={styles.topContainer}>
+                <View style={styles.dataContainer}>
+                    <View style={styles.iconContainer}>
+                        <SubwayIcon />
+                    </View>
+                    <View style={styles.titleContainer}>
+                        <ThemedText style={styles.title}>MRT Blue Line</ThemedText>
+                    </View>
+                </View>
+                <ThemedText style={styles.distance}>1.4 km</ThemedText>
+            </View>
+            <View style={styles.bottomContainer}>
+                <ThemedText style={styles.nearestSubtitle}>Nearest to you: </ThemedText>
+                <ThemedText style={styles.locationSubtitle}>Phahon Yothin</ThemedText>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+export default LineTab;
