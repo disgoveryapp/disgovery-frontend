876ad0caf0477a146501d2d4e4366bcaf0f62ba3
kritasak <jojokritasak@gmail.com>
2022-03-26 11:31:41 +0700

    feat: Face Covering Component

---
+++ pages/route-details/components/face-covering/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import ThemedText from "../../../../components/themed-text";
+import MaskIcon from "../../../../assets/svgs/mask-icon";
+import { useTheme } from "@react-navigation/native";
+
+function FaceCovering(props) {
+    const { colors } = useTheme();
+
+    const styles = StyleSheet.create({
+        container: {
+            width: "100%",
+            maxHeight: 65,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            borderBottomColor: colors.divider,
+            borderBottomWidth: 1,
+            padding: 14,
+        },
+        iconContainer: {
+            display: "flex",
+            marginRight: 8,
+        },
+        title: {
+            flex: 1,
+            fontWeight: "500",
+            fontSize: 14,
+            color: "#FFBF1C",
+        },
+    });
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.iconContainer}>
+                <MaskIcon />
+            </View>
+            <ThemedText style={styles.title}>
+                Face Covering is required when travellingvia public transportation
+            </ThemedText>
+        </View>
+    );
+}
+
+export default FaceCovering;
