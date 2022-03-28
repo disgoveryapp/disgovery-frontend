660b3d5a381178728a4fdbce48e1bd85b8eb52dc
Napat Asavarojkul <puttersnap@icloud.com>
2022-03-28 14:11:12 +0700

    fix: styling

--- pages/route-selection/components/routes-detailed-block/index.js
+++ pages/route-selection/components/routes-detailed-block/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ThemedText from "../../../../components/themed-text";
 import { View, StyleSheet } from "react-native";
 import { useTheme } from "@react-navigation/native";
-
+import WalkIcon from "../../../../assets/svgs/walk-icon";
+import SubwayIcon from "../../../../assets/svgs/subway-icon";
+import { Barlow_100Thin } from "@expo-google-fonts/barlow";
 export default function RoutesDetailedBlock() {
     const { colors } = useTheme();
     const styles = StyleSheet.create({
@@ -10,30 +12,74 @@ export default function RoutesDetailedBlock() {
             width: 350,
             height: 74,
             borderRadius: 12,
+            backgroundColor: colors.upper_background,
+            paddingHorizontal: 12,
+            paddingVertical: 11,
+            justifyContent: "space-between",
+        },
+        arrivedTime: {
+            fontWeight: "500",
+            fontSize: 12,
+        },
+        subContainer: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+        },
+        detailBlock: {
+            alignItems: "flex-end",
+        },
+        subTimeText: {
+            fontSize: 11,
+        },
+        timeText: {
+            fontSize: 16,
+            fontWeight: "600",
+        },
+        iconAndTime: {
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: colors.upper_background,
+            paddingHorizontal: 4,
         },
-        text: {
-            fontWeight: "bold",
-            fontSize: 24,
-            padding: 12,
+        iconBlock: {
+            flexDirection: "row",
+        },
+        detailText: {
+            fontSize: 12,
+            fontWeight: "600",
+        },
+        CompactTransitLineContainer: {
+            width: 36,
+            height: 16,
+            borderRadius: 6,
+            flexDirection: "row",
+            backgroundColor: "green",
+            paddingHorizontal: 3,
+            justifyContent: "space-between",
+            alignItems: "center",
+        },
+        transitIcon: {},
+        transitName: {
+            fontSize: 10,
         },
     });
 
     function CompactTransitLine(type, name) {
         return (
-            <View>
-                <View style={styles.transitIcon}></View>
-                <ThemeText style={styles.transitName}>MRT</ThemeText>
+            <View style={styles.CompactTransitLineContainer}>
+                <View style={styles.transitIcon}>
+                    <SubwayIcon />
+                </View>
+                <ThemedText style={styles.transitName}>MRT</ThemedText>
             </View>
         );
     }
 
-    function iconAndTime(icon, time) {
+    function IconAndTime(icon, time) {
         return (
             <View style={styles.iconAndTime}>
-                <WalkIcon style={styles.icon} />
+                <View styles={styles.icon}>
+                    <CompactTransitLine />
+                </View>
                 <ThemedText style={styles.subTimeText}>11 min</ThemedText>
             </View>
         );
@@ -44,8 +90,9 @@ export default function RoutesDetailedBlock() {
             <ThemedText style={styles.arrivedTime}>Arrive by 16:02</ThemedText>
             <View style={styles.subContainer}>
                 <View style={styles.iconBlock}>
-                    <View styles={styles.icon} />
-                    <ThemedText>Taxi</ThemedText>
+                    <IconAndTime />
+                    <IconAndTime />
+                    <IconAndTime />
                 </View>
                 <View style={styles.detailBlock}>
                     <ThemedText style={styles.timeText}>24 min</ThemedText>
@@ -55,3 +102,4 @@ export default function RoutesDetailedBlock() {
         </View>
     );
 }
+//<WalkIcon />
