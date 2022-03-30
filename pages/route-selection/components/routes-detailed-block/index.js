363dcdf2ec3d2479c973e211229fe9fb61c76cae
Napat Asavarojkul <puttersnap@icloud.com>
2022-03-30 22:25:52 +0700

    add: icon to component

--- pages/route-selection/components/routes-detailed-block/index.js
+++ pages/route-selection/components/routes-detailed-block/index.js
@@ -4,9 +4,14 @@ import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import WalkIcon from "../../../../assets/svgs/walk-icon";
 import SubwayIcon10 from "../../../../assets/svgs/subway-icon-10px";
+import TramIcon10 from "../../../../assets/svgs/tram-icon-10px";
+import RailIcon10 from "../../../../assets/svgs/rail-icon-10px";
+import BusIcon10 from "../../../../assets/svgs/bus-icon-10px";
+import BoatIcon10 from "../../../../assets/svgs/bus-icon-10px";
 
-export default function RoutesDetailedBlock() {
+export default function RoutesDetailedBlock(props) {
     const { colors } = useTheme();
+
     const styles = StyleSheet.create({
         container: {
             width: 350,
@@ -28,7 +33,7 @@ export default function RoutesDetailedBlock() {
         },
         detailBlock: {
             alignItems: "flex-end",
-            justifyContent: "center",
+            justifyContent: "flex-start",
         },
         subTimeText: {
             fontSize: 11,
@@ -67,24 +72,38 @@ export default function RoutesDetailedBlock() {
         },
     });
 
-    function CompactTransitLine(type, name) {
+    function CompactTransitLine(subprops) {
         return (
             <View style={styles.CompactTransitLineContainer}>
                 <View style={styles.transitIcon}>
-                    <SubwayIcon10 fill={colors.text} />
+                    {(subprops.type === "0" || subprops.type === "5") && (
+                        <TramIcon10 fill={colors.text} />
+                    )}
+                    {subprops.type === "1" && <SubwayIcon10 fill={colors.text} />}
+                    {(subprops.type === "2" || subprops.type === "12") && (
+                        <RailIcon10 fill={colors.text} />
+                    )}
+                    {(subprops.type === "3" || subprops.type === "11") && (
+                        <BusIcon10 fill={colors.text} />
+                    )}
+                    {subprops.type === "4" && <BoatIcon10 fill={colors.text} />}
                 </View>
-                <ThemedText style={styles.transitName}>MRT</ThemedText>
+                <ThemedText style={styles.transitName}>{subprops.name}</ThemedText>
             </View>
         );
     }
 
-    function IconAndTime(icon, time) {
+    function IconAndTime(subprops) {
         return (
             <View style={styles.iconAndTime}>
                 <View styles={styles.icon}>
-                    <CompactTransitLine />
+                    {subprops.icon === "walk" ? (
+                        <WalkIcon fill={colors.text} />
+                    ) : (
+                        <CompactTransitLine fill={colors.text} type="1" name="BTS" />
+                    )}
                 </View>
-                <ThemedText style={styles.subTimeText}>11 min</ThemedText>
+                <ThemedText style={styles.subTimeText}>{subprops.time} min</ThemedText>
             </View>
         );
     }
@@ -94,15 +113,11 @@ export default function RoutesDetailedBlock() {
             <ThemedText style={styles.arrivedTime}>Arrive by 16:02</ThemedText>
             <View style={styles.subContainer}>
                 <View style={styles.iconBlock}>
-                    <IconAndTime />
-                    <IconAndTime />
-                    <IconAndTime />
-                    <IconAndTime />
-                    <IconAndTime />
-                    <IconAndTime />
-                    <IconAndTime />
-                    <IconAndTime />
-                    <IconAndTime />
+                    <IconAndTime time="11" />
+                    <IconAndTime icon="walk" time="11" />
+                    <IconAndTime time="11" />
+                    <IconAndTime time="11" />
+                    <IconAndTime time="11" />
                 </View>
                 <View style={styles.detailBlock}>
                     <ThemedText style={styles.timeText}>24 min</ThemedText>
@@ -112,4 +127,33 @@ export default function RoutesDetailedBlock() {
         </TouchableOpacity>
     );
 }
-//<WalkIcon />
+/*
+{(() => {
+                                                                if (
+                                                                    item.type === "0" ||
+                                                                    item.type === "5"
+                                                                ) {
+                                                                    return <TramIcon />;
+                                                                } else if (item.type === "1") {
+                                                                    return <SubwayIcon />;
+                                                                } else if (
+                                                                    item.type === "2" ||
+                                                                    item.type === "12"
+                                                                ) {
+                                                                    return <RailIcon />;
+                                                                } else if (
+                                                                    item.type === "3" ||
+                                                                    item.type === "11"
+                                                                ) {
+                                                                    return <BusIcon />;
+                                                                } else if (item.type === "4") {
+                                                                    return <BoatIcon />;
+                                                                } else {
+                                                                    return (
+                                                                        <PlaceIcon
+                                                                            fill={colors.background}
+                                                                        />
+                                                                    );
+                                                                }
+                                                            })()}
+                                                            */
