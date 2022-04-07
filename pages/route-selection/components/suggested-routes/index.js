98e8641fd187c585cc373ebd01eb7fafdfdcc192
puttersnap <putter3331@gmail.com>
2022-04-07 16:29:19 +0700

    add: props

--- pages/route-selection/components/suggested-routes/index.js
+++ pages/route-selection/components/suggested-routes/index.js
@@ -18,17 +18,27 @@ export default function SuggestedRoutes(props) {
         subContainer: {
             justifyContent: "center",
             alignItems: "center",
+            paddingVertical: 6,
         },
     });
     return (
         <>
             <View style={styles.container}>
                 <ThemedText style={props.topictextStyle}>Suggested routes</ThemedText>
-                <View style={styles.subContainer}>
-                    {props.data.map((item, key) => (
-                        <RoutesDetailedBlock key={key} routeData={item} />
-                    ))}
-                </View>
+
+                {props.data.map((item, key) => (
+                    <View style={styles.subContainer}>
+                        <RoutesDetailedBlock
+                            key={key}
+                            routeData={item}
+                            onPress={() => {
+                                props.setSelectData(item);
+                                props.onPress();
+                            }}
+                        />
+                    </View>
+                ))}
+
                 {HasMostComfy && (
                     <>
                         <ThemedText style={props.topictextStyle}>Most comfortable</ThemedText>
